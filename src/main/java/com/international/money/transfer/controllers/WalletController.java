c100c68414a8d4994bcf087e6f693e721d615fb1
Joao Lages <joaolages.dev@gmail.com>
2025-05-22 22:17:55 -0300

    feat: add wallet controller

---
+++ src/main/java/com/international/money/transfer/controllers/WalletController.java
@@ -0,0 +1,36 @@
+package com.international.money.transfer.controllers;
+
+import com.international.money.transfer.application.useCases.wallet.createWallet.CreateWalletInput;
+import com.international.money.transfer.application.useCases.wallet.createWallet.CreateWalletUseCase;
+import com.international.money.transfer.application.useCases.wallet.getWallets.GetWalletsUseCase;
+import com.international.money.transfer.domain.entities.Wallet;
+import com.international.money.transfer.infrastructure.entities.UserEntity;
+import com.international.money.transfer.infrastructure.entities.WalletEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wallets")
+public class WalletController {
+
+  @Autowired
+  private CreateWalletUseCase createWalletUseCase;
+
+  @Autowired
+  private GetWalletsUseCase getUseCase;
+  @PostMapping
+  public ResponseEntity<Wallet> create(@RequestBody CreateWalletInput input) {
+    Wallet wallet = createWalletUseCase.execute(input);
+    return ResponseEntity.ok(wallet);
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<WalletEntity>> getAll() {
+    List<WalletEntity> wallets = getUseCase.execute(null);
+    return ResponseEntity.ok(wallets);
+  }
+}
